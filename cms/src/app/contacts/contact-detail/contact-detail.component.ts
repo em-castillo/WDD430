b19868166fa01be5c5eebe4bf01504c48d408816
Emily <cas20037@byui.edu>
2024-01-20 10:56:05 -0700

    project done

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Contact} from '../contact.model';
 
 @Component({
   selector: 'app-contact-detail',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './contact-detail.component.css'
 })
 export class ContactDetailComponent {
+  contacts: Contact[] = [
+    new Contact(
+      '1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg', undefined
+    )
+    // new Contact(
+    //   '2', 'R. Rex Barzee Jackson', 'barzeer@byui.edu@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg', undefined
+    // )
+  ];
 
 }
--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -9,10 +9,10 @@ import {Contact} from '../contact.model';
 export class ContactListComponent implements OnInit{
   contacts: Contact[] = [
     new Contact(
-      '1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg'
+      '1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg', undefined
     ),
     new Contact(
-      '2', 'R. Rex Barzee Jackson', 'barzeer@byui.edu@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg'
+      '2', 'R. Rex Barzee Jackson', 'barzeer@byui.edu@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg', undefined
     )
   ];
 
