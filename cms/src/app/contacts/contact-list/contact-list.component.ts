9f7e578c60067efcd603bee118a39d3a7f0b6623
Emily <cas20037@byui.edu>
2024-01-20 06:27:58 -0700

    Contact project

---
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -0,0 +1,23 @@
+import { Component,   OnInit } from '@angular/core';
+import {Contact} from '../contact.model';
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrl: './contact-list.component.css'
+})
+export class ContactListComponent implements OnInit{
+  contacts: Contact[] = [
+    new Contact(
+      '1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg'
+    ),
+    new Contact(
+      '2', 'R. Rex Barzee Jackson', 'barzeer@byui.edu@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg'
+    )
+  ];
+
+  constructor(){}
+
+  ngOnInit(){}
+
+}
